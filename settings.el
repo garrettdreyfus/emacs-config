be87ed49f760854abd93eb6357562bd7d5a3f06f
Garrett Finucane <garrettdreyfus@gmail.com>
2020-10-18 23:37:20 -0700

    Added olivetti correctly

--- settings.el
+++ settings.el
@@ -125,6 +125,18 @@
 (setq bibtex-completion-bibliography (list "/home/garrett/Zotero/library.bib")
 			bibtex-completion-pdf-field "file")
 
+;; (add-hook 'org-mode-hook 'variable-pitch-mode)
+;; (add-hook 'org-agenda-finalize-hook 'variable-pitch-mode)
+ (add-hook 'text-mode-hook 'olivetti-mode)
+ (add-hook 'org-mode-hook 'olivetti-mode)
+ (add-hook 'org-agenda-finalize-hook 'olivetti-mode)
+     (defun org-line-wrap ()
+     (spacemacs/toggle-visual-line-navigation-on)
+     (setq-local word-wrap nil))
+
+ (add-hook 'org-mode-hook 'org-line-wrap)
+ (setq olivetti-body-width 80)
+
 (ivy-mode 1)
 (global-display-line-numbers-mode)
 (global-undo-tree-mode)
