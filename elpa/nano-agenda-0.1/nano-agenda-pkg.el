99433cd9425fce8779695b87ff6410393a2a371f
Garrett Finucane <garrettdreyfus@gmail.com>
2021-10-23 23:11:54 -0700

    New settings

---
+++ elpa/nano-agenda-0.1/nano-agenda-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from nano-agenda.el  -*- no-byte-compile: t -*-
+(define-package "nano-agenda" "0.1" "N Λ N O agenda" '((emacs "27.1")) :maintainer '("Nicolas P. Rougier" . "Nicolas.Rougier@inria.fr") :keywords '("convenience" "org-mode" "org-agenda") :url "https://github.com/rougier/nano-agenda")
